111172168b34d3ad304bb281233996faa68b5783
codes-by-arham <me.arhamjawed@gmail.com>
2026-07-29 13:37:03 +0530

    Express Set Up

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import dotenv from "dotenv";
 import connectDB from "./db/db.js";
 import dns from "dns";
+import { app } from "./app.js";
+import { log } from "console";
 dotenv.config({
   path: "./env",
 });
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
-connectDB();
+connectDB().then(() => {
+  app.listen(process.env.PORT, () => {
+    console.log(`Server Is Running On Port :: ${process.env.PORT}`);
+  });
+});
